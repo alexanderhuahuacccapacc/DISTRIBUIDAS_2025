be7357b4240af2ba61d819733f10c6acbfbea503
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-08-19 12:01:14 -0500

    marac

---
+++ sm_marca/src/main/java/com/example/sm_marca/Repository/MarcaRepository.java
@@ -0,0 +1,7 @@
+package com.example.sm_marca.Repository;
+
+import com.example.sm_marca.Entity.Marca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcaRepository extends JpaRepository<Marca, Integer> {
+}
---
+++ sm_marca/src/main/java/com/example/sm_marca/SmMarcaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sm_marca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmMarcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmMarcaApplication.class, args);
+	}
+
+}
