2bc9d8237cb113c274032ddb9b8d7da3677260e6
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-08-19 12:46:27 -0500

    data_marac

---
+++ msm-config-serve/src/test/java/com/example/msmconfigserve/MsmConfigServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmconfigserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmConfigServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
