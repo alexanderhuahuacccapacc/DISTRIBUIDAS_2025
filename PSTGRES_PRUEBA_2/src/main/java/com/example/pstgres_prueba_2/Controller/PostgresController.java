09e33e7a3315397c11dcb65207eaf482273e6d90
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-08-25 11:54:51 -0500

    postgres_2_k_controller

--- PSTGRES_PRUEBA_2/src/main/java/com/example/pstgres_prueba_2/Controller/PostgresController.java
+++ PSTGRES_PRUEBA_2/src/main/java/com/example/pstgres_prueba_2/Controller/PostgresController.java
@@ -1,4 +1,45 @@
 package com.example.pstgres_prueba_2.Controller;
 
+import com.example.pstgres_prueba_2.Entity.Postg;
+import com.example.pstgres_prueba_2.Service.PostgService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/postg")
 public class PostgresController {
+
+    @Autowired
+    private PostgService postgService ;
+
+    @GetMapping
+    public List<Postg> listar() {
+        return postgService.listar();
+    }
+
+
+    @GetMapping("/{id}")
+    public Optional<Postg> buscarPorId(@PathVariable Integer id) {
+        return postgService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Postg guardar(Postg marca) {
+        return postgService.guardar(marca);
+    }
+
+    @PutMapping
+    public Postg actualizar(Postg marca) {
+        return postgService.actualizar(marca);
+    }
+
+    @DeleteMapping
+    public String eliminar(@PathVariable Integer id) {
+        postgService.borrarPorId(id);
+        return "Marca eliminada";
+    }
 }
