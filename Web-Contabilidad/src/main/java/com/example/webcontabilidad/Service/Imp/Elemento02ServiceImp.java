ae1af83f35155b4aba11761f47a14b7fb913d74c
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-09-15 02:49:35 -0500

    holaaaa_contabilidad-2

--- Web-Contabilidad/src/main/java/com/example/webcontabilidad/Service/Imp/Elemento02ServiceImp.java
+++ Web-Contabilidad/src/main/java/com/example/webcontabilidad/Service/Imp/Elemento02ServiceImp.java
@@ -1,7 +1,19 @@
 package com.example.webcontabilidad.Service.Imp;
 
+import com.example.webcontabilidad.Entity.Elemento02;
+import com.example.webcontabilidad.Repository.Elemento02Repository;
+import com.example.webcontabilidad.Repository.ICrudGenericoRepository;
+import com.example.webcontabilidad.Service.Elemento02Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Elemento02ServiceImp {
+public class Elemento02ServiceImp extends CrudGenericoServiceImp<Elemento02,Long> implements Elemento02Service {
+
+    @Autowired
+    private Elemento02Repository repo2;
+    @Override
+    protected ICrudGenericoRepository<Elemento02, Long> getRepo() {
+        return repo2;
+    }
 }
