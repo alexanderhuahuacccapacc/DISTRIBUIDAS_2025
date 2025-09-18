0318159fd78c7027c8877eef9bc91c3e0f0f78f9
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-09-18 18:42:29 -0500

    Controller12

--- Web-Contabilidad/src/main/java/com/example/webcontabilidad/Controller/Elemento16Controller.java
+++ Web-Contabilidad/src/main/java/com/example/webcontabilidad/Controller/Elemento16Controller.java
@@ -1,4 +1,9 @@
 package com.example.webcontabilidad.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Elemento10")
 public class Elemento16Controller {
 }
