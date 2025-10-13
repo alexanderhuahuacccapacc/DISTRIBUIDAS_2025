b5ec7d30ed36835f160af94eb7e07a1cd56b2446
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-10-13 15:40:18 -0500

    gateway

--- sm_marca/src/main/java/com/example/sm_marca/Repository/MarcaRepository.java
+++ sm_marca/src/main/java/com/example/sm_marca/Repository/MarcaRepository.java
@@ -3,52 +3,6 @@ package com.example.sm_marca.Repository;
 import com.example.sm_marca.Entity.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface
+public interface MarcaRepository extends JpaRepository<Marca, Integer> {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-MarcaRepository extends JpaRepository<Marca, Integer> {
 }
